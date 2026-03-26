d40c47f262e0ca4c063609c6762a37f2d329b288
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2026-03-26 23:35:13 +0530

    Update auth

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,18 +3,20 @@ const router = express.Router();
 
 const authController = require("../controllers/auth.controller");
 
-router.post("/signup", async (req, res) => {
-  console.log("Signup API hit");
+// router.post("/signup", async (req, res) => {
+//   console.log("Signup API hit");
 
-  try {
-    return res.status(200).json({
-      message: "Signup working"
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Error" });
-  }
-});
+//   try {
+//     return res.status(200).json({
+//       message: "Signup working"
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json({ message: "Error" });
+//   }
+// });
+
+router.post("/signup", authController.signup);
 
 router.post("/login", authController.login);
 
