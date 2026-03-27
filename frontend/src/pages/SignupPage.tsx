5d99fa8538ed8ea91c8b434b39b7d2141f9f2b73
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2026-03-27 10:35:00 +0530

    Update signup

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -75,8 +75,8 @@ const SignupPage = () => {
     try {
       setLoading(true);
 
-      // const res = await fetch(`${API_URL}/api/auth/signup`, {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch(`${API_URL}/api/auth/signup`, {
+      // const res = await fetch("http://localhost:5000/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
