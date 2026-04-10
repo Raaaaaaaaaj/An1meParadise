c32435e7a83eae66d1d1fdf797dcda0d9bd42741
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2026-04-10 21:10:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import productImageRoutes from "./routes/productimages.routes.js";
 
 const app = express();
 
+console.log("App starting...");
 // ✅ 1. Trust proxy
 app.set("trust proxy", 1);
 
@@ -35,7 +36,7 @@ app.use(cors({
 // ✅ 4. Logger
 app.use(morgan("dev"));
 
-// ✅ 5. Body parser
+// ✅ 5. Body parser  
 app.use(express.json());
 
 // ✅ 6. Routes
@@ -69,3 +70,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+console.log("Server listening...");
