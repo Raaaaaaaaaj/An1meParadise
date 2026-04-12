88a5f814b8c1aaee9379c0b549fcdba56072e2ab
Aniiiiiiiiiii <banikannwesha@gmail.com>
2026-04-12 12:17:22 +0530

    reverted

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -101,10 +101,9 @@ export const login = async (req, res) => {
       },
     });
   } catch (error) {
-    // res.status(500).json(
-    //   { message: error.message }
-    console.log(errs)
-    // );
+    res.status(500).json(
+      { message: error.message }
+    );
   }
 };
 
