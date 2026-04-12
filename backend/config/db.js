cc263f926074f602980bfd81ee4d8f7ba294a95a
Aniiiiiiiiiii <banikannwesha@gmail.com>
2026-04-13 00:29:06 +0530

    Db js

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,6 +12,9 @@ export const initDB = async () => {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
     });
     console.log("✅ MySQL Connected Successfully");
   } catch (err) {
