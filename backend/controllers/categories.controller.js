a6a9869c63cd6d3db9347b361788190828b648ac
Aniiiiiiiiiii <banikannwesha@gmail.com>
2026-04-21 22:16:16 +0530

    category api update

--- backend/controllers/categories.controller.js
+++ backend/controllers/categories.controller.js
@@ -3,11 +3,21 @@ import { db } from "../config/db.js";
 // ADD CATEGORY
 export const addCategory = async (req, res) => {
   try {
-    const { category_name } = req.body;
+    const { category_name, category_image } = req.body;
 
-    const sql = "INSERT INTO productcategories (category_name) VALUES (?)";
+    if (!category_name) {
+      return res.status(400).json({ message: "Category name is required" });
+    }
 
-    const [result] = await db.query(sql, [category_name]);
+    const sql = `
+      INSERT INTO productcategories (category_name, category_image)
+      VALUES (?, ?)
+    `;
+
+    const [result] = await db.query(sql, [
+      category_name,
+      category_image || null,
+    ]);
 
     res.json({
       message: "Category added",
@@ -17,6 +27,52 @@ export const addCategory = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+//UPDATE CATEGORY
+
+export const updateCategory = async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    const { category_name, category_image } = req.body;
+
+    if (!category_name && !category_image) {
+      return res.status(400).json({
+        message: "At least one field (name or image) is required",
+      });
+    }
+
+    // 🔥 dynamic query build
+    let fields = [];
+    let values = [];
+
+    if (category_name) {
+      fields.push("category_name = ?");
+      values.push(category_name);
+    }
+
+    if (category_image) {
+      fields.push("category_image = ?");
+      values.push(category_image);
+    }
+
+    const sql = `
+      UPDATE productcategories 
+      SET ${fields.join(", ")} 
+      WHERE id = ?
+    `;
+
+    values.push(categoryId);
+
+    await db.query(sql, values);
+
+    res.json({
+      message: "Category updated successfully",
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 
 // GET ALL CATEGORIES
 export const getCategories = async (req, res) => {
--- backend/routes/categories.routes.js
+++ backend/routes/categories.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {addCategory, getCategories, getCategoriesWithCount ,removeCategory} from "../controllers/categories.controller.js"
+import {addCategory, getCategories, getCategoriesWithCount ,removeCategory, updateCategory} from "../controllers/categories.controller.js"
 const router = express.Router();
 
 router.post("/add", addCategory);
 router.get("/", getCategories);
+
+router.put("/update/:id", updateCategory);
 
 router.get("/with-count", getCategoriesWithCount);
 // router.get("/remove", removeCategory);
