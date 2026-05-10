d1685e2cd1c4885e69dca115b3f4b42c42d12b71
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2026-05-10 21:49:32 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use(
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["C ontent-Type", "Authorization"],
   })
 );
 
@@ -88,13 +88,13 @@ app.use((err, req, res, next) => {
 // ✅ 10. Start Server (NO initDB needed)
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => {
-  try {
-    // optional: test DB connection once
-    await db.query("SELECT 1");
-    console.log("✅ DB Pool Connected");
-    console.log(`🚀 Server running on port ${PORT}`);
-  } catch (err) {
-    console.error("❌ DB connection failed:", err.message);
-  }
-});
+// app.listen(PORT, async () => {
+//   try {
+//     // optional: test DB connection once
+//     await db.query("SELECT 1");
+//     console.log("✅ DB Pool Connected");
+//     console.log(`🚀 Server running on port ${PORT}`);
+//   } catch (err) {
+//     console.error("❌ DB connection failed:", err.message);
+//   }
+// });
